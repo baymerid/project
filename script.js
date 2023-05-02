cb7913d4e35caf4460bca1b2828adb3265cc0d8b
baymerid <bay.merdan@gmail.com>
2023-05-02 19:33:42 +0500

    First HomeWork@

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello')
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
+}
+
+for (let i = 0; i < 2; i++) {
+    
+    let lastSeenMovie = prompt('Один из последних просмотренных фильмов?');
+    let lastSeenMovieScore = +prompt('На сколько оцените его?');
+
+    personalMovieDB['movies'][lastSeenMovie] = lastSeenMovieScore;
+}
+
+console.log(personalMovieDB);
